056eb55875a7235680938af0b6f409a624e00ad8
Pedro Furtado <furtado.pedropaulo@gmail.com>
2026-09-21 23:04:53 -0300

    refactor: ajuste no teste de estresse

--- src/test/k6/teste_estresse.js
+++ src/test/k6/teste_estresse.js
@@ -7,19 +7,14 @@ export const options = {
       executor: 'ramping-arrival-rate',
       startRate: 0,
       timeUnit: '1s',
-      preAllocatedVUs: 100,
-      maxVUs: 1000,
+      preAllocatedVUs: 500,
+      maxVUs: 3000,
       stages: [
-        { duration: '10s', target: 500 },
-        { duration: '20s', target: 2000 },
-        { duration: '10s', target: 2000 },
-        { duration: '5s', target: 0 },
+        { duration: '1s', target: 5000 },
+        { duration: '3s', target: 10000 }, /
+        { duration: '1s', target: 0 },
       ],
     },
-  },
-  thresholds: {
-    http_req_failed: ['rate<0.01'],
-    http_req_duration: ['p(95)<1000'],
   },
 };
 
