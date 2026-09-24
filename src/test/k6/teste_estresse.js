59f29bf525d5d8e321b55582e4422c6bf7e54523
Pedro Furtado <furtado.pedropaulo@gmail.com>
2026-09-24 00:20:50 -0300

    feat: alterando teste de estresse

--- src/test/k6/teste_estresse.js
+++ src/test/k6/teste_estresse.js
@@ -2,19 +2,14 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  scenarios: {
-    teste_estresse: {
-      executor: 'ramping-arrival-rate',
-      startRate: 0,
-      timeUnit: '1s',
-      preAllocatedVUs: 500,
-      maxVUs: 3000,
-      stages: [
-        { duration: '1s', target: 5000 },
-        { duration: '3s', target: 10000 },
-        { duration: '1s', target: 0 },
-      ],
-    },
+  stages: [
+    { duration: '5s', target: 200 },  // Sobe para 200 VUs em 5s
+    { duration: '10s', target: 500 }, // Estabiliza em 500 VUs
+    { duration: '5s', target: 0 },    // Baixa
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'],   // Falhas devem ser menor que 1%
+    http_req_duration: ['p(95)<500'], // 95% dos requests abaixo de 500ms
   },
 };
 
@@ -41,7 +36,7 @@ function obterVotoAleatorio() {
 }
 
 export default function () {
-  const url = 'http://localhost:8089/v1/sessoes/1/votos'; // Substitua pelo seu endpoint
+  const url = 'http://localhost:8089/v1/sessoes/3/votos'; // Substitua pelo seu endpoint
 
   const payload = JSON.stringify({
     associadoCpf: gerarCPF(),
